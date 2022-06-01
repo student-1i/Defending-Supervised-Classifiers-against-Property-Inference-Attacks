6298a15c494e9facf2bb39a7004c69c7321e16c9
student-1i <106597827+student-1i@users.noreply.github.com>
2022-06-01 23:52:43 +0800

    Update net.py

--- net.py
+++ net.py
@@ -4,16 +4,12 @@ class Dedend_Net(nn.Module):
         super(Dedend_Net, self).__init__()
         self.fc1 = nn.Linear(41, 32)
         torch.nn.init.xavier_uniform_(self.fc1.weight)
-
         self.fc2 = nn.Linear(32, 16)
         torch.nn.init.xavier_uniform_(self.fc2.weight)
-
         self.fc3 = nn.Linear(16, 8)
         torch.nn.init.xavier_uniform_(self.fc3.weight)
-
         self.fc4 = nn.Linear(8, 2)
         torch.nn.init.xavier_uniform_(self.fc4.weight)
-
     def forward(self, x):
         x = F.leaky_relu(self.fc1(x))
         x = F.leaky_relu(self.fc2(x))
